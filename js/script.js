84ba90dc68df8c45ae7c80f5deea4eed26552b36
SmirnofOleg <super.smirnofoleg@yandex.ru>
2019-04-20 10:04:52 +0300

    появилась недологика в search

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
+// временное меню
 $(document).ready(function() {
 	$(".burger").on("click", function(e){
 		e.preventDefault();
 		$(".nav-menu").toggleClass("nav-active");
 	})
 });
+
+// наведение на search 
+
+	var search = document.getElementById('search');
+	var btn = document.getElementById('search_submit');
+
+	// btn.onmouseover = function(){
+	// 	this.style.background = 'yellow';
+	// }
+
+	// btn.onmouseout = function(){
+	// 	this.style.background = 'transparent';
+	// }
+btn.onmouseover = menuShow;
+// btn.onmouseout = menuHide;
+
+function menuShow(){
+	search.style.width = '90px';
+}
+function menuHide(){
+	search.style.width = '0px';
+}
+
+
+
+
