e86a1a70f456bcef814854b5a6d45a93723c05cd
SmirnofOleg <super.smirnofoleg@yandex.ru>
2019-04-26 20:43:06 +0300

    Обновил меню

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+$(document).ready(function(){
+	var link = $('.menu-link');
+	var link_active = $('.menu-link_actve');
+	var menu = $('.nav-menu');
+	var nav_link = $('.menu a')//убираем меню по клику на ссылку
 
+	link.click(function(){
+		link.toggleClass('menu-link_active');
+		menu.toggleClass('nav-menu_active');
+	});
+	
+	nav_link.click(function(){
+		link.toggleClass('menu-link_active');
+		menu.toggleClass('menu_active');
+	});
+});
 
 // временное меню
-$(document).ready(function() {
-	$(".burger").on("click", function(e){
-		e.preventDefault();
-		$(".nav-menu").toggleClass("nav-active");
-	})
-});
+// $(document).ready(function() {
+// 	$(".burger").on("click", function(e){
+// 		e.preventDefault();
+// 		$(".nav-menu").toggleClass("nav-active");
+// 	})
+// });
 
 // наведение на search 
 	var search = document.getElementById('search');
