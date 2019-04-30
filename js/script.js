ed881fb4e0d1fd4917385aed9c17cfbfd2376a60
SmirnofOleg <super.smirnofoleg@yandex.ru>
2019-04-30 22:54:05 +0300

    добавил блок services и начал следующий блок

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// меню в адаптиве
 $(document).ready(function(){
 	var link = $('.menu-link');
 	var link_active = $('.menu-link_actve');
@@ -33,35 +34,28 @@ $(document).ready(function(){
 	/*настраиваем слайдер*/
 	$('.slider-block').slick();
 
-	// эффект с иконками на слайде
 
-	$('.slider-text').mousemove(function(e) {
 
-		var width = $(this).width() / 2;
-		var height = $(this).height() / 2;
-	
-		var amountMovedX1 = ((width - e.pageX) * -1 / 16);
-		var amountMovedY1 = ((height - e.pageY) * -1 / 16);
-	
-		var amountMovedX2 = ((width - e.pageX) / 40);
-		var amountMovedY2 = ((height - e.pageY) / 30);
-	
-		var amountMovedX3 = ((width - e.pageX) / 20);
-		var amountMovedY3 = ((height - e.pageY) / 20);
-	
-		var amountMovedX4 = ((width - e.pageX) * -1  / 10);
-		var amountMovedY4 = ((height - e.pageY) * -1  / 20);
-	
-		$('.shapes1').css('marginLeft', amountMovedX1);
-		$('.shapes1').css('marginTop', amountMovedY1);
-	
-		$('.shapes2').css('marginLeft', amountMovedX2);
-		$('.shapes2').css('marginTop', amountMovedY2);
-	
-		$('.shapes3').css('marginLeft', amountMovedX3);
-		$('.shapes3').css('marginTop', amountMovedY3);
-	
-		$('.shapes4').css('marginLeft', amountMovedX4);
-		$('.shapes4').css('marginTop', amountMovedY4);
-
-	})
+	/*настраиваем слайдер блок services*/
+	$('.services__container').slick({
+		infinite: true,
+  		slidesToShow: 4,
+  		slidesToScroll: 1,
+		  responsive: [
+    
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+	});
